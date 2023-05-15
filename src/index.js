e043b6a9dbc365b5153801743babd48cb43fb91a
felipeslopes <felipesl.lopes@hotmail.com>
2023-05-15 16:35:13 -0300

    atualizando projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { Feather, FontAwesome } from "@expo/vector-icons";
 import { Camera, CameraType, } from "expo-camera";
-import { useRef, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, } from "react-native";
+import * as Permissions from "expo-permissions";
+import { useEffect, useRef, useState } from "react";
+import { Image, Modal, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as MediaLibrary from "expo-media-library"
 
 export default function Home() {
 
@@ -10,22 +12,32 @@ export default function Home() {
     const camRef = useRef(null);
     const [capturedPhoto, setCapturedPhoto] = useState();
     const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off)
+    const [open, setOpen] = useState(false);
 
-    if (!permission) {
-        return <View />
-    }
+    useEffect(() => {
+        (async () => {
 
-    if (!permission.granted) {
-        return <Text>Acesso negado!</Text>
-    }
+            if (!permission) {
+                return <View />
+            }
+
+            if (!permission.granted) {
+                return <Text>Acesso negado!</Text>
+            }
+
+            // const { status } = Permissions.MEDIA_LIBRARY
+            // console.log(status)
+
+        })();
+    }, [])
+
+
 
     async function takePicture() {
         if (camRef) {
             const data = await camRef.current.takePictureAsync();
-            setCapturedPhoto(data.uri)
-            console.log("\n\n", data)
-            console.log(flashMode)
-            console.log(type)
+            setCapturedPhoto(data.uri);
+            setOpen(true);
         }
     }
 
@@ -38,9 +50,13 @@ export default function Home() {
         setFlashMode(current => (current === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.torch : Camera.Constants.FlashMode.off))
     }
 
+    async function savePicture() {
+        MediaLibrary.saveToLibraryAsync(capturedPhoto)
+        setOpen(false)
+    }
 
     return (
-        <View style={styles.container} >
+        <SafeAreaView style={styles.container} >
 
             <Camera
                 ref={camRef}
@@ -66,7 +82,34 @@ export default function Home() {
                 </TouchableOpacity>
             </View>
 
-        </View>
+            {capturedPhoto &&
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={open}
+                >
+                    <View style={styles.modalPhoto}>
+
+                        <View style={styles.modalButtons}>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => setOpen(false)}>
+                                <FontAwesome name="window-close" size={40} color={"red"} />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.modalButton} onPress={savePicture}>
+                                <FontAwesome name="upload" size={40} color={"white"} />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Image
+                            style={styles.photo}
+                            source={{ uri: capturedPhoto }}
+                        />
+
+                    </View>
+                </Modal>
+            }
+
+        </SafeAreaView>
     )
 }
 
@@ -94,5 +137,21 @@ const styles = StyleSheet.create({
         height: 54,
         width: 54,
         borderRadius: 30
+    },
+    modalPhoto: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20,
+    },
+    modalButtons: {
+        flexDirection: 'row',
+    },
+    modalButton: {
+        margin: 10,
+    },
+    photo: {
+        width: '100%',
+        height: '85%',
     },
 })
